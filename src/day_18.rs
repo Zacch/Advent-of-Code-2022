cc9bbe8cadba790199044781107101740710c598
Rolf Staflin <github@piro.se>
2022-12-18 14:54:30 +0100

    Day 18 part 1 done
    I started on a breadth first search to find all the external cube faces, but I think it's easier to just fill the drop shape instead

--- src/day_18.rs
+++ src/day_18.rs
@@ -1,12 +1,173 @@
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::fs;
 
+use crate::points::{CubeFace, Direction, Point3};
+use crate::points::Direction::*;
 use crate::traits::StringExtensions;
 
 pub fn run() {
     println!("Day 18");
     let contents = fs::read_to_string("input/day_18.txt").expect("File missing");
 
+    let mut cubes: HashMap<String, Point3> = HashMap::new();
     for line in contents.lines() {
-        let _coords = line.to_int_vector();
+        let cube = Point3::from_vector(line.to_int_vector());
+        cubes.insert(cube.to_string(), cube);
+    }
+
+    let mut part1 = 0;
+    for cube in cubes.values() {
+        if !cubes.contains_key(&cube.left().to_string()) { part1 += 1 }
+        if !cubes.contains_key(&cube.right().to_string()) { part1 += 1 }
+        if !cubes.contains_key(&cube.up().to_string()) { part1 += 1 }
+        if !cubes.contains_key(&cube.down().to_string()) { part1 += 1 }
+        if !cubes.contains_key(&cube.forward().to_string()) { part1 += 1 }
+        if !cubes.contains_key(&cube.back().to_string()) { part1 += 1 }
+    }
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2(&cubes));
+
+}
+
+fn part2(cubes: &HashMap<String, Point3>) -> usize {
+    let max_z = cubes.values().map(|p| p.z).max().unwrap();
+    let first_cube = cubes.values().next().unwrap();
+    let mut current_cube = Point3::new(first_cube.x, first_cube.y, max_z);
+    while !cubes.contains_key(&current_cube.to_string()) {
+        current_cube = current_cube.back();
+    }
+    let face = CubeFace::new(current_cube, Forward);
+
+    println!("{:?}", current_cube);
+    println!("{:?}", face);
+
+    let mut faces: Vec<CubeFace> = vec![face];
+
+    let mut frontier: VecDeque<CubeFace> = VecDeque::new();
+    frontier.push_back(face);
+    let mut visited: HashSet<CubeFace> = HashSet::new();
+
+    while !frontier.is_empty() {
+     //   println!("-------------------------");
+        let current = frontier.pop_front().unwrap();
+       // println!("current   {:?}", current);
+        let neighbors = neighbors_of(current);
+        for next in neighbors {
+            if visited.contains(&next) { continue; }
+            if !cubes.contains_key(&next.cube.to_string()) { continue; }
+            if cubes.contains_key(&next.cube.go(face.direction).to_string()) { continue; }
+
+       //     println!("Adding {:?}", next);
+            faces.push(next);
+            frontier.push_back(next);
+        }
+        println!("Inserting {:?}", current);
+        visited.insert(current);
+    }
+
+    visited.len()
+}
+
+//noinspection DuplicatedCode
+fn neighbors_of(face: CubeFace) -> Vec<CubeFace> {
+
+    let facing_cube = face.cube.go(face.direction);
+    match face.direction {
+        Up => {
+            vec![
+                CubeFace::new(facing_cube, Down),
+                CubeFace::new(face.cube, Left),
+                CubeFace::new(facing_cube, Left),
+                CubeFace::new(face.cube.left(), Up),
+                CubeFace::new(face.cube, Right),
+                CubeFace::new(facing_cube, Right),
+                CubeFace::new(face.cube.right(), Up),
+                CubeFace::new(face.cube, Forward),
+                CubeFace::new(facing_cube, Forward),
+                CubeFace::new(face.cube.forward(), Up),
+                CubeFace::new(face.cube, Back),
+                CubeFace::new(facing_cube, Back),
+                CubeFace::new(face.cube.back(), Up),
+        ]},
+        Down => {
+            vec![
+                CubeFace::new(facing_cube, Up),
+                CubeFace::new(face.cube, Left),
+                CubeFace::new(facing_cube, Left),
+                CubeFace::new(face.cube.left(), Down),
+                CubeFace::new(face.cube, Right),
+                CubeFace::new(facing_cube, Right),
+                CubeFace::new(face.cube.right(), Down),
+                CubeFace::new(face.cube, Forward),
+                CubeFace::new(facing_cube, Forward),
+                CubeFace::new(face.cube.forward(), Down),
+                CubeFace::new(face.cube, Back),
+                CubeFace::new(facing_cube, Back),
+                CubeFace::new(face.cube.back(), Down),
+            ]},
+        Left => {
+            vec![
+                CubeFace::new(facing_cube, Right),
+                CubeFace::new(face.cube, Up),
+                CubeFace::new(facing_cube, Up),
+                CubeFace::new(face.cube.up(), Left),
+                CubeFace::new(face.cube, Down),
+                CubeFace::new(facing_cube, Down),
+                CubeFace::new(face.cube.down(), Left),
+                CubeFace::new(face.cube, Forward),
+                CubeFace::new(facing_cube, Forward),
+                CubeFace::new(face.cube.forward(), Left),
+                CubeFace::new(face.cube, Back),
+                CubeFace::new(facing_cube, Back),
+                CubeFace::new(face.cube.back(), Left),
+            ]},
+        Right => {
+            vec![
+                CubeFace::new(facing_cube, Left),
+                CubeFace::new(face.cube, Up),
+                CubeFace::new(facing_cube, Up),
+                CubeFace::new(face.cube.up(), Right),
+                CubeFace::new(face.cube, Down),
+                CubeFace::new(facing_cube, Down),
+                CubeFace::new(face.cube.down(), Right),
+                CubeFace::new(face.cube, Forward),
+                CubeFace::new(facing_cube, Forward),
+                CubeFace::new(face.cube.forward(), Right),
+                CubeFace::new(face.cube, Back),
+                CubeFace::new(facing_cube, Back),
+                CubeFace::new(face.cube.back(), Right),
+            ]},
+        Forward => {
+            vec![
+                CubeFace::new(facing_cube, Back),
+                CubeFace::new(face.cube, Left),
+                CubeFace::new(facing_cube, Left),
+                CubeFace::new(face.cube.left(), Forward),
+                CubeFace::new(face.cube, Right),
+                CubeFace::new(facing_cube, Right),
+                CubeFace::new(face.cube.right(), Forward),
+                CubeFace::new(face.cube, Up),
+                CubeFace::new(facing_cube, Up),
+                CubeFace::new(face.cube.up(), Forward),
+                CubeFace::new(face.cube, Down),
+                CubeFace::new(facing_cube, Down),
+                CubeFace::new(face.cube.down(), Forward),
+            ]},
+        Back => {
+            vec![
+                CubeFace::new(facing_cube, Forward),
+                CubeFace::new(face.cube, Left),
+                CubeFace::new(facing_cube, Left),
+                CubeFace::new(face.cube.left(), Back),
+                CubeFace::new(face.cube, Right),
+                CubeFace::new(facing_cube, Right),
+                CubeFace::new(face.cube.right(), Back),
+                CubeFace::new(face.cube, Up),
+                CubeFace::new(facing_cube, Up),
+                CubeFace::new(face.cube.up(), Back),
+                CubeFace::new(face.cube, Down),
+                CubeFace::new(facing_cube, Down),
+                CubeFace::new(face.cube.down(), Back),
+            ]},
     }
 }
--- src/points.rs
+++ src/points.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Display, Formatter};
 use std::ops;
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
@@ -8,19 +9,11 @@ pub struct Point {
 
 impl Point {
     pub fn origin() -> Point { Point{ x: 0, y: 0} }
-    pub fn new(x: i32, y: i32) -> Point {
-        Point { x, y }
-    }
+    pub fn new(x: i32, y: i32) -> Point { Point { x, y } }
     pub fn up(&self) -> Point { Point::new(self.x, self.y + 1) }
-    pub fn down(&self) -> Point {
-        Point::new(self.x, self.y - 1)
-    }
-    pub fn left(&self) -> Point {
-        Point::new(self.x - 1, self.y)
-    }
-    pub fn right(&self) -> Point {
-        Point::new(self.x + 1, self.y)
-    }
+    pub fn down(&self) -> Point { Point::new(self.x, self.y - 1) }
+    pub fn left(&self) -> Point { Point::new(self.x - 1, self.y) }
+    pub fn right(&self) -> Point { Point::new(self.x + 1, self.y) }
 
     pub fn n(&self) -> Point { Point::new(self.x, self.y + 1) }
     pub fn s(&self) -> Point { Point::new(self.x, self.y - 1) }
@@ -42,4 +35,61 @@ impl Point {
 impl ops::Add<&Point> for Point {
     type Output = Point;
     fn add(self, rhs: &Point) -> Point { Point::new(self.x + rhs.x, self.y + rhs.y) }
+}
+
+//----------------------------------------------------------------------------
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+pub struct Point3 {
+    pub x: i32,
+    pub y: i32,
+    pub z: i32,
+}
+
+
+impl Point3 {
+    pub fn origin() -> Point3 { Point3{ x: 0, y: 0, z: 0 } }
+    pub fn new(x: i32, y: i32, z: i32) -> Point3 { Point3 { x, y, z } }
+    pub fn from_vector(v: Vec<i32>) -> Point3 { Point3::new(v[0], v[1], v[2]) }
+
+    pub fn up(&self) -> Point3 { Point3::new(self.x, self.y + 1, self.z) }
+    pub fn down(&self) -> Point3 { Point3::new(self.x, self.y - 1, self.z) }
+    pub fn left(&self) -> Point3 { Point3::new(self.x - 1, self.y, self.z) }
+    pub fn right(&self) -> Point3 { Point3::new(self.x + 1, self.y, self.z) }
+    pub fn forward(&self) -> Point3 { Point3::new(self.x, self.y, self.z + 1) }
+    pub fn back(&self) -> Point3 { Point3::new(self.x, self.y, self.z - 1) }
+
+    pub fn go(&self, direction: Direction) -> Point3 {
+        match direction {
+            Direction::Up => { self.up() }
+            Direction::Down => { self.down() }
+            Direction::Left => { self.left() }
+            Direction::Right => { self.right() }
+            Direction::Forward => { self.forward() }
+            Direction::Back => { self.back() }
+        }
+    }
+
+    pub fn manhattan_distance(&self, p: &Point3) -> i32 {
+        i32::abs(self.x - p.x) + i32::abs(self.y - p.y) + i32::abs(self.z - p.z)
+    }
+}
+
+impl Display for Point3 {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{},{},{}]", self.x, self.y, self.z)
+    }
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+pub enum Direction { Up, Down, Left, Right, Forward, Back }
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+pub struct CubeFace {
+    pub cube: Point3,
+    pub direction: Direction,
+}
+
+impl CubeFace {
+    pub fn new(cube: Point3, direction: Direction) -> CubeFace { CubeFace { cube, direction } }
 }
