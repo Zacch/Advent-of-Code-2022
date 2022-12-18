bf0dc4769e356ea5418d74460bc1d0ee65f7176c
Rolf Staflin <github@piro.se>
2022-12-18 12:47:31 +0100

    Day 17 done

--- src/day_17.rs
+++ src/day_17.rs
@@ -14,77 +14,62 @@ pub fn run() {
         vec![Point::new(0,0), Point::new(0, 1), Point::new(0, 2), Point::new(0, 3)],
         vec![Point::new(0,0), Point::new(1, 0), Point::new(0, 1), Point::new(1, 1)],
     ];
-    let mut chamber: Vec<Vec<char>> = vec![vec!['.'; 7]; 100000];
-    let mut highest_point: i64 = 0;
+    let mut chamber: Vec<Vec<char>> = vec![vec!['.'; 7]; 1000000];
+    let mut column_heights: Vec<i64> = vec![0; 7];
+    let mut highest_point: i64;
     let (mut next_jet, mut next_shape) = (0, 0);
 
-    let mut visited: HashMap<usize, i64> = HashMap::new();
-    let mut heights: Vec<i64> = vec![0];
-    let mut height_per_cycle: i64 = 0;
-    let mut cycle_start: i64 = 0;
-    let mut cycle_length: i64 = 0;
+    let mut highest_column_history: Vec<i64> = vec![0];
+    let mut history: HashMap<String, i64> = HashMap::new();
+
+    let height_per_cycle: i64;
+    let cycle_start: i64;
+    let cycle_length: i64;
 
     let mut turn = 1;
-    let mut stop_turn: i64 = 1000000000000000;
+    loop {
+        drop_shape(&jets, &shapes, &mut chamber, &mut column_heights, &mut next_jet, &mut next_shape);
+        highest_point = *column_heights.iter().max().unwrap();
 
-    let mut draw_count = 0;
-    let mut draw_from = stop_turn;
-    while turn < stop_turn {
-        drop_shape(&jets, &shapes, &mut chamber, &mut highest_point, &mut next_jet, &mut next_shape);
-
-        if draw_count > 0 || turn >= draw_from {
-            println!("{:?}", turn);
-            draw_chamber(&chamber, highest_point);
-            draw_count -= 1;
+        let mut this_key = "".to_string();
+        for i in 0..7 {
+            this_key += &(50 + column_heights[i] - column_heights[0]).to_string();
         }
-        heights.push(highest_point);
-        let key = next_jet * 100 + next_shape;
-        // println!("{}: jet {} shape {}, key {}", turn, next_jet, next_shape, key);
-        if visited.contains_key(&key) && cycle_start == 0 {
-            cycle_start = visited[&key] as i64;
-            height_per_cycle = highest_point - heights[cycle_start as usize];
+
+        let full_key = next_shape.to_string() + &next_jet.to_string() + &this_key.to_owned();
+        if history.contains_key(&full_key) {
+            cycle_start = history[&full_key];
+            height_per_cycle = highest_point - highest_column_history[cycle_start as usize];
             cycle_length = turn - cycle_start;
-            stop_turn = 2 * turn + 10;
-            draw_from = stop_turn - 7;
-            draw_count = 7;
+            break;
         }
-        visited.insert(key, turn);
+        history.insert(full_key, turn);
+        highest_column_history.push(highest_point);
+
         turn += 1;
     }
-/*
-    println!("cycle_start {:?}", cycle_start);
-    println!("height_per_cycle {:?}", height_per_cycle);
-    println!("cycle_length {:?}", cycle_length);
-    for i in (cycle_start + cycle_length - 5)..turn {
-        println!("{}: {:?}", i, heights[i as usize]);
-    }
-
-    The problem is that the top of the pile of rocks is very different before and after the first cycle.
- */
-
 
     let mut cycles: i64 = (2022 - cycle_start) / cycle_length;
     let mut rest: i64 = (2022 - cycle_start) % cycle_length;
-    println!("Part 1: {:?}", heights[(cycle_start + rest) as usize] + cycles * height_per_cycle);
+    println!("Part 1: {:?}", highest_column_history[(cycle_start + rest) as usize] + cycles * height_per_cycle);
 
     cycles = (1000000000000 - cycle_start) / cycle_length;
     rest = (1000000000000 - cycle_start) % cycle_length;
-    println!("Part 2: {:?}", heights[(cycle_start + rest) as usize] + cycles * height_per_cycle);
-
+    println!("Part 2: {:?}", highest_column_history[(cycle_start + rest) as usize] + cycles * height_per_cycle);
 }
 
 
 fn drop_shape(jets: &Vec<char>, shapes: &Vec<Vec<Point>>,
-              chamber: &mut [Vec<char>], highest_point: &mut i64,
+              chamber: &mut [Vec<char>], column_heights: &mut [i64],
               next_jet: &mut usize, next_shape: &mut usize) {
 
+    let highest_point = column_heights.iter().max().unwrap();
     let mut shape_origin = Point::new(2, (*highest_point + 4) as i32);
     let shape = &shapes[*next_shape];
     *next_shape = (*next_shape + 1) % shapes.len();
     let shape_width = 1 + shape.iter().map(|p| p.x).max().unwrap();
     let mut shape_is_falling = true;
 
-    // println!("{:?} for {:?}", shape_origin, shape);
     while shape_is_falling {
         let jet = jets[*next_jet];
         *next_jet = (*next_jet + 1) % jets.len();
@@ -97,20 +82,18 @@ fn drop_shape(jets: &Vec<char>, shapes: &Vec<Vec<Point>>,
                 chamber[(p.y + shape_origin.y) as usize][(p.x + shape_origin.x + 1) as usize] == '.') {
             shape_origin.x += 1;
         }
-        // println!("{} => {:?}", jet, shape_origin);
 
         if shape.iter().any(|p| p.y + shape_origin.y == 1 ||
                 chamber[(p.y + shape_origin.y - 1) as usize][(p.x + shape_origin.x) as usize] == '#') {
             shape_is_falling = false;
-            // println!("Stop. origin == {:?}", shape_origin);
             for p in shape {
                 let chamber_point = *p + &shape_origin;
                 chamber[chamber_point.y as usize][chamber_point.x as usize] = '#';
-                *highest_point = (*highest_point).max(chamber_point.y as i64);
+                column_heights[chamber_point.x as usize] =
+                    column_heights[chamber_point.x as usize].max(chamber_point.y as i64);
             }
         } else {
             shape_origin.y -= 1;
-            // println!("v => {:?}", shape_origin);
         }
     }
 }
