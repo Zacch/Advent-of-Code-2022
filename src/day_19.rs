577a3efe0e90abfa260d7556fe15dbeee651eccc
Rolf Staflin <github@piro.se>
2022-12-20 00:40:12 +0100

    Day 19 part 1 with added heuristics

--- src/day_19.rs
+++ src/day_19.rs
@@ -5,7 +5,7 @@ use std::fs;
 
 use crate::traits::StringExtensions;
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+#[derive(Debug, Eq, PartialEq, Hash)]
 struct Recipe {
     id: i32,
     stop_time: i32,
@@ -15,12 +15,13 @@ struct Recipe {
     obsidian_robot_clay_cost: i32,
     geode_robot_ore_cost: i32,
     geode_robot_obsidian_cost: i32,
+    max_ore_cost: i32,
 }
 
 impl Recipe {
-
     pub fn from_string(line: &str) -> Recipe {
         let ints =line.to_int_vector();
+        let ore_costs = vec![ints[1], ints[2], ints[3], ints[5]];
         Recipe {
             id: ints[0],
             stop_time: 24,
@@ -30,35 +31,12 @@ impl Recipe {
             obsidian_robot_clay_cost: ints[4],
             geode_robot_ore_cost: ints[5],
             geode_robot_obsidian_cost: ints[6],
+            max_ore_cost: *ore_costs.iter().max().unwrap(),
         }
     }
 }
 
-pub fn run() {
-    println!("Day 19");
-    let contents = fs::read_to_string("input/day_19.txt").expect("File missing");
-
-    let mut recipes: Vec<Recipe> = vec![];
-    for line in contents.lines() {
-        recipes.push(Recipe::from_string(line));
-    }
-
-    let mut part1 = 0;
-    for recipe in recipes.iter() {
-        part1 += recipe.id * max_geode_production(*recipe)
-    }
-    println!("Part 1: {}", part1);
-
-    let mut part2 = 1;
-    for i in 0..2 {
-        let mut new_recipe = recipes[i];
-        new_recipe.stop_time = 32;
-        part2 *= max_geode_production(new_recipe);
-    }
-    println!("Part 2: {}", part2);
-
-}
-
+// ------------------------
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 struct State {
     time: i32,
@@ -70,6 +48,14 @@ struct State {
     clay_bots: i32,
     obsidian_bots: i32,
     geode_bots: i32,
+}
+
+impl Display for State {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{}:{},{},{},{}-{},{},{},{}]",
+               self.time, self.ore, self.clay, self.obsidian, self.geodes,
+               self.ore_bots, self.clay_bots, self.obsidian_bots, self.geodes)
+    }
 }
 
 impl PartialOrd for State {
@@ -90,43 +76,69 @@ impl Ord for State {
     fn cmp(&self, other: &Self) -> Ordering { self.partial_cmp(other).unwrap() }
 }
 
-impl Display for State {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "[{}:{},{},{},{}-{},{},{},{}]",
-               self.time, self.ore, self.clay, self.obsidian, self.geodes,
-                self.ore_bots, self.clay_bots, self.obsidian_bots, self.geodes)
+// ------------------------
+pub fn run() {
+    println!("Day 19");
+    let contents = fs::read_to_string("input/day_19.txt").expect("File missing");
+
+    let mut recipes: Vec<Recipe> = vec![];
+    for line in contents.lines() {
+        recipes.push(Recipe::from_string(line));
     }
+
+    let mut part1 = 0;
+    for recipe in recipes.iter() {
+        part1 += recipe.id * max_geode_production(recipe)
+    }
+    println!("Part 1: {}", part1);
+/*
+    let mut part2 = 1;
+    for i in 0..2 {
+        let mut new_recipe = recipes[i];
+        new_recipe.stop_time = 32;
+        part2 *= max_geode_production(new_recipe);
+    }
+    println!("Part 2: {}", part2);
+*/
 }
 
-fn max_geode_production(recipe: Recipe) -> i32 {
+fn max_geode_production(recipe: &Recipe) -> i32 {
     let start = State {
         time: 0, ore: 0, clay: 0, obsidian: 0, geodes: 0,
         ore_bots: 1, clay_bots: 0, obsidian_bots: 0, geode_bots: 0, };
 
-    let mut memos: HashMap<String, State> = HashMap::new();
-    let best_end_result: State = dfs(recipe, start, &mut memos);
+    let mut memos: HashMap<State, State> = HashMap::new();
+    let mut best_results_so_far = vec![0; (recipe.stop_time + 1) as usize];
+    let best_end_result: State = dfs(recipe, &start, &mut memos, &mut best_results_so_far);
 
     println!("Best end result for recipe {:?}", recipe.id);
     println!("{:?}", best_end_result);
     best_end_result.geodes
 }
 
-fn dfs(recipe: Recipe, state: State, memos: &mut HashMap<String, State>) -> State {
-    let state_key = state.to_string();
-    if memos.contains_key(&state_key) { return memos[&state_key]; }
-    if state.time == recipe.stop_time { memos.insert(state_key, state); return state; }
+fn dfs(recipe: &Recipe, state: &State, memos: &mut HashMap<State, State>, best_so_far: &mut Vec<i32>) -> State {
+    if memos.contains_key(state) { return memos[state]; }
+    if state.time == recipe.stop_time { memos.insert(*state, *state); return *state; }
 
-    let descendants: Vec<State> = descendants(recipe, state);
+    // Give up this branch if the best solution seen is better than we can possibly achieve
+    let time_left = recipe.stop_time - state.time;
+    let theoretical_max = time_left * (state.geode_bots + time_left - 1) + state.geodes;
+    if theoretical_max < best_so_far[state.time as usize] { return *state; }
+
+    let children: Vec<State> = children(recipe, state);
     let mut results: Vec<State> = vec![];
-    for next in descendants {
-        results.push(dfs(recipe, next, memos));
+    for next in children.iter() {
+        results.push(dfs(recipe, next, memos, best_so_far));
     }
     let best = *results.iter().max().unwrap();
-    memos.insert(state_key, best);
+    if best_so_far[state.time as usize] < best.geodes {
+        best_so_far[state.time as usize] = best.geodes;
+    }
+    memos.insert(*state, best);
     best
 }
 
-fn descendants(recipe: Recipe, state: State) -> Vec<State> {
+fn children(recipe: &Recipe, state: &State) -> Vec<State> {
     let no_action = State {
         time: state.time + 1,
         ore: state.ore + state.ore_bots,
@@ -140,8 +152,9 @@ fn descendants(recipe: Recipe, state: State) -> Vec<State> {
     };
     let mut result: Vec<State> = vec![];
 
+    // Always build a geode cracking robot if possible
     if recipe.geode_robot_obsidian_cost <= state.obsidian && recipe.geode_robot_ore_cost <= state.ore {
-        result.push(State {
+        return vec![State {
             time: no_action.time,
             ore: no_action.ore - recipe.geode_robot_ore_cost,
             clay: no_action.clay,
@@ -151,10 +164,13 @@ fn descendants(recipe: Recipe, state: State) -> Vec<State> {
             clay_bots: no_action.clay_bots,
             obsidian_bots: no_action.obsidian_bots,
             geode_bots: no_action.geode_bots + 1,
-        })
+        }];
     }
 
-    if recipe.obsidian_robot_clay_cost <= state.clay && recipe.obsidian_robot_ore_cost <= state.ore {
+    if recipe.obsidian_robot_clay_cost <= state.clay &&
+        recipe.obsidian_robot_ore_cost <= state.ore &&
+        state.obsidian_bots < recipe.geode_robot_obsidian_cost
+    {
         result.push(State {
             time: no_action.time,
             ore: no_action.ore - recipe.obsidian_robot_ore_cost,
@@ -168,7 +184,9 @@ fn descendants(recipe: Recipe, state: State) -> Vec<State> {
         })
     }
 
-    if recipe.clay_robot_cost <= state.ore {
+    if recipe.clay_robot_cost <= state.ore  &&
+        state.clay_bots < recipe.obsidian_robot_clay_cost
+    {
         result.push(State {
             time: no_action.time,
             ore: no_action.ore - recipe.clay_robot_cost,
@@ -182,7 +200,8 @@ fn descendants(recipe: Recipe, state: State) -> Vec<State> {
         })
     }
 
-    if recipe.ore_robot_cost <= state.ore {
+    if recipe.ore_robot_cost <= state.ore &&
+        state.ore_bots < recipe.max_ore_cost {
         result.push(State {
             time: no_action.time,
             ore: no_action.ore - recipe.ore_robot_cost,
